93b33f4ee1940151aab8c54297a773babc1e0c92
Cody Stoltman <particlebanana@gmail.com>
2013-06-03 15:39:20 -0500

    add waterline-adapter-tests

---
+++ test/integration/runner.js
@@ -0,0 +1,28 @@
+/**
+ * Run Integration Tests
+ *
+ * Uses the waterline-adapter-tests module to
+ * run mocha tests against the currently implemented
+ * waterline API.
+ */
+
+var tests = require('waterline-adapter-tests'),
+    adapter = require('../../MySQLAdapter'),
+    mocha = require('mocha');
+
+/**
+ * Build a MySQL Config File
+ */
+
+var config = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sails_mysql'
+};
+
+/**
+ * Run Tests
+ */
+
+var suite = new tests({ adapter: adapter, config: config });

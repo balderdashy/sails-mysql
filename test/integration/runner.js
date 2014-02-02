9dff7c036ff878a4387def0883dd0bb4559a2484
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-01 20:06:27 -0600

    Normalize the test runner.

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -1,37 +1,99 @@
 /**
- * Run Integration Tests
+ * Run integration tests
  *
- * Uses the waterline-adapter-tests module to
- * run mocha tests against the currently implemented
- * waterline API.
+ * Uses the `waterline-adapter-tests` module to
+ * run mocha tests against the appropriate version
+ * of Waterline.  Only the interfaces explicitly
+ * declared in this adapter's `package.json` file
+ * are tested. (e.g. `queryable`, `semantic`, etc.)
  */
 
-var tests = require('waterline-adapter-tests'),
-    adapter = require('../../lib/adapter'),
-    mocha = require('mocha');
 
 /**
- * Build a MySQL Config File
+ * Module dependencies
  */
 
-var config = {
-  host: process.env.WATERLINE_ADAPTER_TESTS_HOST || 'localhost',
-  user: process.env.WATERLINE_ADAPTER_TESTS_USER || 'root',
-  password: process.env.WATERLINE_ADAPTER_TESTS_PASSWORD || '',
-  database: process.env.WATERLINE_ADAPTER_TESTS_DATABASE || 'sails_mysql',
-  pool: true,
-  connectionLimit: 10,
-  waitForConnections: true
-};
+var util = require('util');
+var mocha = require('mocha');
+var log = require('captains-log')();
+var TestRunner = require('waterline-adapter-tests');
+var Adapter = require('../../lib/adapter');
+
+
+
+// Grab targeted interfaces from this adapter's `package.json` file:
+var package = {},
+interfaces = [];
+try {
+	package = require('../../package.json');
+  interfaces = package.waterlineAdapter.interfaces;
+}
+catch (e) {
+    throw new Error(
+    '\n'+
+    'Could not read supported interfaces from `waterlineAdapter.interfaces`'+'\n' +
+    'in this adapter\'s `package.json` file ::' + '\n' +
+    util.inspect(e)
+    );
+}
+
+
+
+
+
+log.info('Testing `' + package.name + '`, a Sails/Waterline adapter.');
+log.info('Running `waterline-adapter-tests` against ' + interfaces.length + ' interfaces...');
+log.info('( ' + interfaces.join(', ') + ' )');
+console.log();
+log('Latest draft of Waterline adapter interface spec:');
+log('https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md');
+console.log();
+
+
 
 /**
- * Expose Interfaces Used In Adapter
+ * Integration Test Runner
+ *
+ * Uses the `waterline-adapter-tests` module to
+ * run mocha tests against the specified interfaces
+ * of the currently-implemented Waterline adapter API.
  */
+new TestRunner({
 
-var interfaces = ['semantic', 'queryable', 'migratable', 'associations'];
+    // Load the adapter module.
+    adapter: Adapter,
 
-/**
- * Run Tests
- */
+    // Default connection config to use.
+    config: {
+		  host: process.env.WATERLINE_ADAPTER_TESTS_HOST || 'localhost',
+		  user: process.env.WATERLINE_ADAPTER_TESTS_USER || 'root',
+		  password: process.env.WATERLINE_ADAPTER_TESTS_PASSWORD || '',
+		  database: process.env.WATERLINE_ADAPTER_TESTS_DATABASE || 'sails_mysql',
+		  pool: true,
+		  connectionLimit: 10,
+		  waitForConnections: true
+		},
 
-var suite = new tests({ adapter: adapter, config: config, interfaces: interfaces });
+    // The set of adapter interfaces to test against.
+    // (grabbed these from this adapter's package.json file above)
+    interfaces: interfaces
+
+    // Most databases implement 'semantic' and 'queryable'.
+    //
+    // As of Sails/Waterline v0.10, the 'associations' interface
+    // is also available.  If you don't implement 'associations',
+    // it will be polyfilled for you by Waterline core.  The core
+    // implementation will always be used for cross-adapter / cross-connection
+    // joins.
+    //
+    // In future versions of Sails/Waterline, 'queryable' may be also
+    // be polyfilled by core.
+    //
+    // These polyfilled implementations can usually be further optimized at the
+    // adapter level, since most databases provide optimizations for internal
+    // operations.
+    //
+    // Full interface reference:
+    // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
+});
+

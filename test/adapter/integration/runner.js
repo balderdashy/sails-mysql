a23fc301b931f57e0103083df7c8c67a7a1e1f3e
Cody Stoltman <particlebanana@gmail.com>
2016-12-22 18:33:48 -0600

    setup integration runner

--- test/adapter/integration/runner.js
+++ test/adapter/integration/runner.js
@@ -14,18 +14,17 @@
  */
 
 var util = require('util');
-var mocha = require('mocha');
 var TestRunner = require('waterline-adapter-tests');
-var Adapter = require('../../lib/adapter');
-
+var Adapter = require('../../../lib/adapter');
 
 
 // Grab targeted interfaces from this adapter's `package.json` file:
-var package = {},
-  interfaces = [],
-  features = [];
+var package = {};
+var interfaces = [];
+var features = [];
+
 try {
-  package = require('../../package.json');
+  package = require('../../../package.json');
   interfaces = package.waterlineAdapter.interfaces;
   features = package.waterlineAdapter.features;
 } catch (e) {
@@ -38,29 +37,27 @@ try {
 }
 
 
-
 console.log('Testing `' + package.name + '`, a Sails/Waterline adapter.');
 console.log('Running `waterline-adapter-tests` against ' + interfaces.length + ' interfaces...');
 console.log('( ' + interfaces.join(', ') + ' )');
 console.log();
 console.log('Latest draft of Waterline adapter interface spec:');
-console.log('http://sailsjs.com/documentation/concepts/extending-sails/adapters');
+console.log('http://links.sailsjs.org/docs/plugins/adapters/interfaces');
 console.log();
 
 
-
-/**
- * Integration Test Runner
- *
- * Uses the `waterline-adapter-tests` module to
- * run mocha tests against the specified interfaces
- * of the currently-implemented Waterline adapter API.
- */
+// //////////////////////////////////////////////////////////////////////
+// Integration Test Runner
+//
+// Uses the `waterline-adapter-tests` module to
+// run mocha tests against the specified interfaces
+// of the currently-implemented Waterline adapter API.
+// //////////////////////////////////////////////////////////////////////
 new TestRunner({
 
   // Mocha opts
   mocha: {
-    bail: true
+    bail: false
   },
 
   // Load the adapter module.
@@ -68,17 +65,15 @@ new TestRunner({
 
   // Default connection config to use.
   config: {
-    host: process.env.MYSQL_PORT_3306_TCP_ADDR || process.env.WATERLINE_ADAPTER_TESTS_HOST || 'localhost',
-    port: process.env.WATERLINE_ADAPTER_TESTS_PORT || 3306,
-    user: process.env.MYSQL_ENV_MYSQL_USER || process.env.WATERLINE_ADAPTER_TESTS_USER || 'root',
-    password: process.env.MYSQL_ENV_MYSQL_PASSWORD || process.env.WATERLINE_ADAPTER_TESTS_PASSWORD || '',
-    database: process.env.MYSQL_ENV_MYSQL_DATABASE || process.env.WATERLINE_ADAPTER_TESTS_DATABASE || 'sails_mysql',
-    pool: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    waitForConnections: true
+    host: process.env.POSTGRES_1_PORT_5432_TCP_ADDR || process.env.WATERLINE_ADAPTER_TESTS_HOST || 'localhost',
+    user: process.env.POSTGRES_ENV_POSTGRES_USER || process.env.WATERLINE_ADAPTER_TESTS_USER || 'sails',
+    password: process.env.POSTGRES_ENV_POSTGRES_PASSWORD || process.env.WATERLINE_ADAPTER_TESTS_PASSWORD || 'sails',
+    database: process.env.POSTGRES_ENV_POSTGRES_DB || process.env.WATERLINE_ADAPTER_TESTS_DATABASE || 'sailspg',
+    port: process.env.POSTGRES_PORT_5432_TCP_PORT || process.env.WATERLINE_ADAPTER_TESTS_PORT || 5432,
+    schema: true
   },
 
+  failOnError: true,
   // The set of adapter interfaces to test against.
   // (grabbed these from this adapter's package.json file above)
   interfaces: interfaces,
@@ -86,9 +81,6 @@ new TestRunner({
   // The set of adapter features to test against.
   // (grabbed these from this adapter's package.json file above)
   features: features,
-
-  // Return code non zero if any test fails
-  failOnError: true
 
   // Most databases implement 'semantic' and 'queryable'.
   //
@@ -106,5 +98,5 @@ new TestRunner({
   // operations.
   //
   // Full interface reference:
-  // http://sailsjs.com/documentation/concepts/extending-sails/adapters
+  // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
 });

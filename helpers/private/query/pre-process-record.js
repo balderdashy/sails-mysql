3b91083a3382d7099093d10152caecc7744808bb
Mike McNeil <mikermcneil@users.noreply.github.com>
2017-02-10 01:42:20 -0600

    Carve out a special exception for the null literal when running JSON.stringify() on incoming values for type: 'json' attrs, and also when decoding outgoing values w/ JSON.parse().

--- helpers/private/query/pre-process-record.js
+++ helpers/private/query/pre-process-record.js
@@ -5,18 +5,31 @@
 //  ██║     ██║  ██║███████╗    ██║     ██║  ██║╚██████╔╝╚██████╗███████╗███████║███████║
 //  ╚═╝     ╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚══════╝╚══════╝╚══════╝
 //
-//  ██████╗ ███████╗ ██████╗ ██████╗ ██████╗ ██████╗
-//  ██╔══██╗██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
-//  ██████╔╝█████╗  ██║     ██║   ██║██████╔╝██║  ██║
-//  ██╔══██╗██╔══╝  ██║     ██║   ██║██╔══██╗██║  ██║
-//  ██║  ██║███████╗╚██████╗╚██████╔╝██║  ██║██████╔╝
-//  ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝
+//  ██████╗ ███████╗ ██████╗ ██████╗ ██████╗ ██████╗  SSSSSS
+//  ██╔══██╗██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗ S
+//  ██████╔╝█████╗  ██║     ██║   ██║██████╔╝██║  ██║ SSSSSS
+//  ██╔══██╗██╔══╝  ██║     ██║   ██║██╔══██╗██║  ██║      S
+//  ██║  ██║███████╗╚██████╗╚██████╔╝██║  ██║██████╔╝      S
+//  ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ SSSSSSS
 //
 
 var _ = require('@sailshq/lodash');
 var utils = require('waterline-utils');
 var eachRecordDeep = utils.eachRecordDeep;
 
+
+
+/**
+ * [exports description]
+ *
+ * TODO: Document this utility
+ *
+ * TODO: change the name of this utility to reflect the fact that its job is
+ * to pre-process new incoming records (plural)
+ *
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
 module.exports = function preProcessRecord(options) {
   //  ╦  ╦╔═╗╦  ╦╔╦╗╔═╗╔╦╗╔═╗  ┌─┐┌─┐┌┬┐┬┌─┐┌┐┌┌─┐
   //  ╚╗╔╝╠═╣║  ║ ║║╠═╣ ║ ║╣   │ │├─┘ │ ││ ││││└─┐
@@ -37,13 +50,23 @@ module.exports = function preProcessRecord(options) {
     throw new Error('Invalid option used in options argument. Missing or invalid orm.');
   }
 
-  // Run all the records through the iterator so that they can be normalized.
+  // Run all the new, incoming records through the iterator so that they can be normalized
+  // with anything adapter-specific before getting written to the database.
   eachRecordDeep(options.records, function iterator(record, WLModel) {
-    // JSON stringify any type of JSON attributes because MySQL can't store JSON.
     _.each(WLModel.definition, function checkAttributes(attrDef, attrName) {
+
+      // JSON stringify the values provided for any `type: 'json'` attributes
+      // because MySQL can't store JSON.
       if (attrDef.type === 'json' && _.has(record, attrName)) {
-        record[attrName] = JSON.stringify(record[attrName]);
-      }
+
+        // Special case: If this is the `null` literal, leave it alone.
+        // But otherwise, stringify it into a JSON string.
+        // (even if it's already a string!)
+        if (!_.isNull(record[attrName])) {
+          record[attrName] = JSON.stringify(record[attrName]);
+        }
+
+      }//>-
 
       // If the attribute is type ref and not a Buffer then don't allow it.
       if (attrDef.type === 'ref' && _.has(record, attrName) && !_.isNull(record[attrName])) {
